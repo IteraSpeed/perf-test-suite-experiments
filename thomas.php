fa4ea7d18c53030cef6767deb1f14fe494700812
ThomasHeigl <thnew2@gmail.com>
2015-04-29 15:52:53 +0200

    Moved script tags to bottom

--- thomas.php
+++ thomas.php
@@ -19,25 +19,6 @@
 	<!-- Custom CSS -->
 	<link href="assets/css/header.css" rel="stylesheet">
 	<link href="assets/css/main.css" rel="stylesheet">
-    
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
-    <!-- jQuery Version 1.11.0 -->
-	<script src="js/jquery-1.11.0.js"></script>
-	<script src="js/jquery-ui.js"></script>
-	<!-- Bootstrap Core JavaScript -->
-	<script src="js/bootstrap.js"></script>
-	<script src="js/jquery.magnific-popup.js"></script>
-	<script src="js/shuffle.js"></script>
-	<script src="js/jquery.shapeshift.js"></script>
-	<script src="js/homepage.js"></script>
-
 </head>
 
 <body>
@@ -825,8 +806,26 @@
         </footer>
     </div>
     <!-- /.container -->
+	
+	
+	
 
-    
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+    <!-- jQuery Version 1.11.0 -->
+	<script src="js/jquery-1.11.0.js"></script>
+	<script src="js/jquery-ui.js"></script>
+	<!-- Bootstrap Core JavaScript -->
+	<script src="js/bootstrap.js"></script>
+	<script src="js/jquery.magnific-popup.js"></script>
+	<script src="js/shuffle.js"></script>
+	<script src="js/jquery.shapeshift.js"></script>
+	<script src="js/homepage.js"></script>
 
     <script src="js/postPictureLoad.js"></script>
     <script src="js/profiler.js"></script>

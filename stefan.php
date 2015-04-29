f829b7a460ecc2119e1ee0dbd9e0d552bcd2bf00
SCALE-Team <sliesem@Stefans-MacBook-Air.local>
2015-04-29 14:40:21 +0200

    Changed Headline

--- stefan.php
+++ stefan.php
@@ -106,7 +106,7 @@
     <!-- Page Content -->
     <div class="container page" id="home">
 
-        <h2>Example shop page</h2>
+        <h2>Example shop page (Stefan)</h2>
 
         <!-- carrousel -->
         <div class="row">

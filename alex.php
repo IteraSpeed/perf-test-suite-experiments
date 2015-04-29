bdc5a0fee482412f8e1e470da2f88ddfedcc992d
Alexander Mas <alexander.mas@gmail.com>
2015-04-29 16:34:16 +0200

    Fix JS

--- alex.php
+++ alex.php
@@ -21,7 +21,15 @@
 	<link href="assets/css/main.css" rel="stylesheet">
     <link href="assets/css/spritesheet.css" rel="stylesheet">
 
-
+    <!-- jQuery Version 1.11.0 -->
+    <script src="js/jquery-1.11.0.js"></script>
+    <script src="js/jquery-ui.js"></script>
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.js"></script>
+    <script src="js/jquery.magnific-popup.js"></script>
+    <script src="js/shuffle.js"></script>
+    <script src="js/jquery.shapeshift.js"></script>
+    <script src="js/homepage.js"></script>
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
@@ -804,16 +812,6 @@
         </footer>
     </div>
     <!-- /.container -->
-    <!-- jQuery Version 1.11.0 -->
-    <script src="js/jquery-1.11.0.js"></script>
-    <script src="js/jquery-ui.js"></script>
-    <!-- Bootstrap Core JavaScript -->
-    <script src="js/bootstrap.js"></script>
-    <script src="js/jquery.magnific-popup.js"></script>
-    <script src="js/shuffle.js"></script>
-    <script src="js/jquery.shapeshift.js"></script>
-    <script src="js/homepage.js"></script>
-    
     <script src="js/postPictureLoad.js"></script>
     <script src="js/profiler.js"></script>
     <script>
